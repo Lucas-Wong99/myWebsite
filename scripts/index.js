2f9e5e2692006cc923645da13c0f40dc743675ec
Lucas <lucaswong.ca@gmail.com>
2020-08-10 18:43:17 -0700

    introduce color coordination

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,23 @@
 $(document).ready(() => {
+  $(window).on("load", () => {
+    $(".title").animate(
+      {
+        opacity: 1
+      },
+      2000
+    );
+  });
+
   $(".hidden-info").hide(0);
 
   $(".show-info-button").click(() => {
-    $(".hidden-info").toggle(1000);
+    $(".show-info-button").fadeOut(1000);
+    $(".hidden-info").show(1000);
+    $(".hidden-info").animate(
+      {
+        opacity: 1
+      },
+      1000
+    );
   });
 });
