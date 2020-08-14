dbf660a662c40febe97814830383d26380a0d1dc
Lucas <lucaswong.ca@gmail.com>
2020-08-13 17:49:33 -0700

    Intersection observer additions and style changes

--- scripts/observer.js
+++ scripts/observer.js
@@ -1,18 +1,48 @@
-const skillsSection = document.querySelector("#skills-sec");
-const sections = document.querySelectorAll("section");
+const nav = document.querySelector("nav");
+const main = document.querySelector(".main-container");
 
-const options = {
-  root: null,
-  threshold: 0,
-  rootMargin: "-100px"
+const fadeIn = document.querySelectorAll(".image");
+
+const sliders = document.querySelectorAll(".slider");
+
+const navOptions = {
+  rootMargin: "0px 0px -600px 0px"
 };
 
-const observer = new IntersectionObserver(function (entries, observer) {
+const navObserver = new IntersectionObserver(function (entries, aboutObserver) {
   entries.forEach((entry) => {
-    // console.log(entry.target);
+    if (entry.isIntersecting) {
+      nav.classList.add("nav-scrolled");
+    } else {
+      nav.classList.remove("nav-scrolled");
+    }
   });
-}, options);
+}, navOptions);
 
-sections.forEach((section) => {
-  observer.observe(section);
+navObserver.observe(main);
+
+const appearOptions = {
+  threshold: 0.5
+};
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+fadeIn.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
 });
